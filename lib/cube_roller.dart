ea9907cec06f8c782fd584c66211482a764da000
Bartosz Azymi <bartosz.azymi@gmail.com>
2024-06-10 17:52:18 +0200

    BoxShadow&Fixes

--- lib/cube_roller.dart
+++ lib/cube_roller.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 final randomizer = Random();
@@ -28,27 +27,57 @@ class _CubeRollerState extends State<CubeRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(
-          activeCubeImagine,
-          width: 201,
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.8),
+                spreadRadius: -85,
+                blurRadius: 20,
+                offset: const Offset(0, 0), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Image.asset(
+            activeCubeImagine,
+            width: 350,
+          ),
         ),
         const SizedBox(height: 50),
         TextButton(
           onPressed: rollDice,
           style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: const Color.fromARGB(255, 10, 119, 255),
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 48.0),
-              textStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              )),
+            foregroundColor: Colors.white,
+            backgroundColor: const Color.fromARGB(255, 10, 119, 255),
+            padding: const EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 48.0,
+            ),
+            textStyle: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
           child: const Text(
             'Roll The Dice',
           ),
-        )
+        ),
       ],
     );
   }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cube Roller'),
+        ),
+        body: const Center(
+          child: CubeRoller(),
+        ),
+      ),
+    ),
+  );
 }
